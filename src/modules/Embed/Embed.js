4508bf5a4562da02f7fb458675e9c5af7cf45497
Alexander Fedyashov <af@e42.guru>
2016-11-04 18:05:40 +0200

    feat(Embed): Add component

--- src/modules/Embed/Embed.js
+++ src/modules/Embed/Embed.js
@@ -7,25 +7,70 @@ import {
   getUnhandledProps,
   META,
   SUI,
+  useKeyOnly,
 } from '../../lib'
+import Icon from '../../elements/Icon'
 
 /**
  * An embed displays content from other websites like YouTube videos or Google Maps.
  */
 export default class Embed extends Component {
+  static autoControlledProps = [
+    'active',
+  ]
+
+  static defaultProps = {
+    icon: 'video play',
+  }
+
+  handleClick = () => this.trySetState({ active: true })
+
+  renderEmbed() {
+    const { children, source, sourceId } = this.props
+
+    if (children) return <div>{children}</div>
+
+    if (source === 'youtube') {
+      const src = [
+        `//www.youtube.com/embed/${sourceId}?autohide=true&amp;autoplay=true&amp;color=%23444444&amp;hq=true&amp;`,
+        'jsapi=false&amp;modestbranding=true',
+      ].join('')
+
+      return (
+        <div className='embed'>
+          <iframe
+            allowFullScreen=''
+            frameBorder='0'
+            height='100%'
+            scrolling='no'
+            src={src}
+            width='100%'
+          />
+        </div>
+      )
+    }
+  }
 
   render() {
-    const { className } = this.props
+    const { className, icon, placeholder } = this.props
+    const { active } = this.state
 
     const classes = cx(
       'ui',
-      'ember',
+      useKeyOnly(active, 'active'),
+      'embed',
       className,
     )
     const rest = getUnhandledProps(Embed, this.props)
     const ElementType = getElementType(Embed, this.props)
 
-    return <ElementType {...rest} className={classes} />
+    return (
+      <ElementType {...rest} className={classes} onClick={this.handleClick}>
+        {Icon.create(icon)}
+        <img className='placeholder' src={placeholder} />
+        {active && this.renderEmbed()}
+      </ElementType>
+    )
   }
 }
 
@@ -40,4 +85,12 @@ Embed.propTypes = {
 
   /** Additional classes. */
   className: PropTypes.string,
+
+  icon: PropTypes.string,
+
+  placeholder: PropTypes.string,
+
+  source: PropTypes.oneOf(['youtube', 'vimeo']),
+
+  sourceId: PropTypes.string,
 }
